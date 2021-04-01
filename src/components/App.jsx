cf68e3c753555461ebdd6928c178566edd360431
nileshbohra <nmbohra2001@gmail.com>
2021-04-01 11:42:10 +0530

    ui improved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,23 +10,34 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const backend_url = process.env.REACT_APP_BACKEND_URL;
 
+  //retreving data from backend
+  //called on first render
+  useEffect(() => {
+    Axios.get(`${backend_url}/read`).then((res) => {
+      setItems(res.data);
+      setIsLoaded(true);
+    });
+  }, []);
+
   //posting data to DB through backend
   function addNote(note) {
-    return Axios.post(`${backend_url}/create`, {
+    Axios.post(`${backend_url}/create`, {
       title: note.title,
       content: note.content,
     });
+    //retreving new data when note added
+    Axios.get(`${backend_url}/read`).then((res) => {
+      setItems(res.data);
+    });
   }
-
-  //retreving data from backend
-  useEffect(() => {
-    Axios.get(`${backend_url}/read`).then((res) => setItems(res.data));
-    setIsLoaded(true);
-  }, [items]);
 
   //deleting data from DB through backend
   function deleteNote(id) {
     Axios.delete(`${backend_url}/delete/${id}`);
+    //retreving new data when note deleted
+    Axios.get(`${backend_url}/read`).then((res) => {
+      setItems(res.data);
+    });
   }
 
   return (
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,33 +1,58 @@
 import React, { useState } from "react";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
-    content: ""
-  })
+    content: "",
+  });
 
-  function handleChange(e){
-    const {name, value} = e.target;
-    return setNote(prevValue => {
-      return {...prevValue, [name]: value}
-    })
+  function handleChange(e) {
+    const { name, value } = e.target;
+    return setNote((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
   }
 
-  function submitNote(e){
-    props.addNote(note)
+  function submitNote(e) {
+    props.addNote(note);
     setNote({
       title: "",
-      content: ""
-    })
-    e.preventDefault()
+      content: "",
+    });
+    e.preventDefault();
   }
+
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <div>
       <form>
-        <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
-        <textarea onChange={handleChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isClicked} timeout={1000}>
+          <input
+            style={{ display: isClicked ? null : "none" }}
+            onChange={handleChange}
+            value={note.title}
+            name="title"
+            placeholder="Title"
+          />
+        </Zoom>
+        <textarea
+          onClick={() => setIsClicked(true)}
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows= {isClicked ? "3" : "1"}
+        />
+        <Zoom in={isClicked} timeout={1000}>
+          <button
+            style={{ display: isClicked ? null : "none" }}
+            onClick={submitNote}
+          >
+            Add
+          </button>
+        </Zoom>
       </form>
     </div>
   );
